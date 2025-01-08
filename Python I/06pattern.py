114fee6281069c3a4425ad15056d3a793d59e35c
Idries Ahamed K A <127777022+1drie5@users.noreply.github.com>
2025-01-08 19:26:44 +0530

    pattern

---
+++ Python I/06pattern.py
@@ -0,0 +1,13 @@
+'''
+Write a program to display the following numbers: 
+5678 
+678 
+78 
+8 
+where the given number is 5678.
+'''
+num = int(input("Enter only four digit number "))
+print(num)
+print(num%1000)
+print(num%100)
+print(num%10)
