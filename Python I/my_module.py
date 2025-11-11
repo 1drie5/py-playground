c70d89bffb0e3cf6840e1a7d4030eb7ced1e1f11
Idries Ahamed K A <idriesahamedka@gmail.com>
2025-11-12 00:58:56 +0530

    add is_prime in my_module

--- Python I/my_module.py
+++ Python I/my_module.py
@@ -1,2 +1,10 @@
 def is_palindrome(s):
     return s == s[::-1]
+
+def is_prime(number):
+    if number <= 1:
+        return False
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
