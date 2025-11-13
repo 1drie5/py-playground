8e723ce5890a435f957ea060f128fa67c6a28c3e
Idries Ahamed K A <idriesahamedka@gmail.com>
2025-11-13 20:45:02 +0530

    add factorial in my_module

--- Python I/my_module.py
+++ Python I/my_module.py
@@ -8,3 +8,11 @@ def is_prime(number):
         if number % i == 0:
             return False
     return True
+
+def factorial(number):
+    if number < 0:
+        return None
+    result = 1
+    for i in range(1, number + 1):
+        result *= i
+    return result
